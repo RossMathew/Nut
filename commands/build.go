fccd8a6a7a89612c4a8fb21179a37cce73875243
Ranjib Dey <ranjib@linux.com>
2016-02-13 12:06:16 -0800

    remove publishing and archiving features from build

--- commands/build.go
+++ commands/build.go
@@ -22,10 +22,7 @@ func (command *BuildCommand) Help() string {
 		-specfile    Local path to the specification file (defaults to dockerfle)
 		-ephemeral   Destroy the container after creation
 		-name        Name of the container (defaults to randomly generated UUID)
-		-stop        Stop container at the end
 		-volume      Mount host directory inside container
-		-export      Create tarball of container rootfs (assumes -stop)
-		-publish     Publish tarball in S3
 	`
 	return strings.TrimSpace(helpText)
 }
@@ -41,29 +38,11 @@ func (command *BuildCommand) Run(args []string) int {
 	flagSet.Usage = func() { fmt.Println(command.Help()) }
 
 	file := flagSet.String("specfile", "Dockerfile", "Container build specification file")
-	stopAfterBuild := flagSet.Bool("stop", false, "Stop container after build")
 	ephemeral := flagSet.Bool("ephemeral", false, "Destroy the container after creating it")
 	name := flagSet.String("name", "", "Name of the resulting container (defaults to randomly generated UUID)")
-	export := flagSet.String("export", "", "File path for the container tarball")
-	exportSudo := flagSet.Bool("export-sudo", false, "Use sudo while invoking tar")
 	volume := flagSet.String("volume", "", "Mount host directory inside container. Format: '[host_directory:]container_directory[:mount options]")
-	publish := flagSet.String("publish", "", "Publish tarball in s3 (assumes -stop, -export)")
 
 	flagSet.Parse(args)
-	if *publish != "" {
-		if *export == "" {
-			uuid, err := specification.UUID()
-			if err != nil {
-				log.Errorln(err)
-				return -1
-			}
-			*export = uuid
-		}
-	}
-
-	if *export != "" {
-		*stopAfterBuild = true
-	}
 	if *name == "" {
 		uuid, err := specification.UUID()
 		if err != nil {
@@ -85,35 +64,10 @@ func (command *BuildCommand) Run(args []string) int {
 		return -1
 	}
 
-	if *stopAfterBuild {
-		log.Infof("Stopping container")
-		if err := spec.Stop(); err != nil {
-			log.Errorf("Failed to stop container. Error: %s\n", err)
-			return -1
-		}
-	}
-
-	if *export != "" {
-		log.Infof("Exporting container")
-		if err := spec.Export(*export, *exportSudo); err != nil {
-			log.Errorf("Failed to export container. Error: %s\n", err)
-			return -1
-		}
-	}
-
 	if *ephemeral {
 		log.Infof("Ephemeral mode. Destroying the container")
 		if err := spec.Destroy(); err != nil {
 			log.Errorf("Failed to destroy container. Error: %s\n", err)
-			return -1
-		}
-	}
-	if *publish != "" {
-		parts := strings.Split(*publish, "/")
-		bucket := parts[0]
-		key := strings.Join(parts[1:], "/")
-		if err := specification.Publish(*export, "us-west-1", bucket, key); err != nil {
-			log.Errorln(err)
 			return -1
 		}
 	}
